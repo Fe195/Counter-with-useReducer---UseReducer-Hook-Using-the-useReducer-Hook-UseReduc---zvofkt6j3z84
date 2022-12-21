dda2c2bc7055df185f0f5df48283523a0bbbd0b0
Feba Rachel Lalu <114395573+Fe195@users.noreply.github.com>
2022-12-21 22:58:05 +0530

    Update counterReducer.js

--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -1,5 +1,38 @@
-const counterReducer = (state,action) =>{
-
+const signUpFormReducer = (state, action) => {
+    switch (action.type) {
+        case 'input':
+            return {
+                ...state,
+                input:{
+                    ...state.input,
+                    [action.payload.field]: action.payload.value
+                }      
+            }
+        case 'error':
+            return {
+                ...state,
+                errors:{
+                    ...state.errors,
+                    [action.payload.field]: action.payload.value
+                }      
+            }
+        case 'resetErrors':
+            const errors = {}
+            for(const t in state.errors){
+                errors[t] = ""
+            }
+            return {
+                ...state,
+                errors
+            }
+        case 'toggleConsent':
+            return {
+                ...state,
+                consent: !state.consent
+            }
+        default:
+            break;
+    }
 }
 
-export {counterReducer}
+export { signUpFormReducer }
